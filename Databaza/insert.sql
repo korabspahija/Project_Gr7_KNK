c652efe8e5861262d29d9c580344afb2290ed9f5
Visar7 <visar.buza7@gmail.com>
2019-05-16 23:53:46 +0200

    insert

---
+++ Databaza/insert.sql
@@ -0,0 +1,61 @@
+INSERT INTO `sistemiautobuseve`.`roles` (`role_name`) VALUES ('Admin');
+INSERT INTO `sistemiautobuseve`.`roles` (`role_name`) VALUES ('Company');
+
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Prishtina');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Peja');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Prizreni');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Mitrovica');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Gjilani');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Ferizaj');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Gjakova');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Klina');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Istogu');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Suhareka');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Skenderaj');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Malisheva');
+INSERT INTO `sistemiautobuseve`.`cities` (`name`) VALUES ('Decani');
+
+
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('06:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('06:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('07:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('07:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('08:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('08:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('09:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('09:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('10:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('10:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('11:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('11:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('12:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('12:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('13:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('13:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('14:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('14:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('15:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('15:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('16:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('16:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('17:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('17:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('18:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('18:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('19:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('19:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('20:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('20:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('21:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('21:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('22:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('22:30');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('23:00');
+INSERT INTO `sistemiautobuseve`.`timetable` (`time`) VALUES ('23:30');
+
+
+
+
+
+
+
