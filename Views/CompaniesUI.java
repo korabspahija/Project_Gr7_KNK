ee908fa356379964e5cd24b0800a6e697dbec9fb
shpatgashi <44480572+shpatgashi@users.noreply.github.com>
2019-06-11 16:45:50 +0200

    Update
    Ju pershendes yjzi

--- Views/CompaniesUI.java
+++ Views/CompaniesUI.java
@@ -11,35 +11,64 @@ import javafx.scene.layout.VBox;
 
 import java.awt.print.Book;
 
-
-public class CompaniesUI  extends HBox{
+class CompaniesUI  extends HBox{
 
     private TextField txtID = new TextField();
-    private TextField txtStart  = new TextField();;
-    private TextField txtEnd  = new TextField();
+    private TextField txtTime  = new TextField();
+    private TextField txtPrice = new TextField();
+    private ComboBox<String> cboStart = new ComboBox<>();
+    private ComboBox<String> cboEnd = new ComboBox<>();
 
     private Button btnSearch = new Button("Search");
     private Button btnAdd = new Button("Add");
     private Button btnUpdate = new Button("Update");
     private Button btnDelete = new Button("Delete");
+
     private TableView tabela = new TableView();
 
     private VBox leftPane = new VBox();
     private GridPane formPane = new GridPane();
     private HBox hBox = new HBox();
 
-    CompaniesUI(){
+    public CompaniesUI(){
+
+        txtID.setDisable(true);
+        cboStart.setValue("Starting point");
+        cboEnd.setValue("Ending point");
+
+        // caktimi i width
+        txtID.setMaxWidth(150);
+        txtPrice.setMaxWidth(150);
+        txtTime.setMaxWidth(150);
+        cboStart.setMaxWidth(150);
+        cboEnd.setMaxWidth(150);
+
+
+        // krijimi i formes ??
         formPane.addRow(0,new Label("ID :"), txtID);
-        formPane.addRow(1,new Label("Start :"), txtStart);
-        formPane.addRow(2,new Label("End :"), txtEnd);
+        formPane.addRow(1,new Label("Start :"), cboStart);
+        formPane.addRow(2,new Label("End :"), cboEnd);
+        formPane.addRow(3,new Label("Time :"), txtTime);
+        formPane.addRow(4,new Label("Price :"), txtPrice);
+
+        formPane.setVgap(10);
+        formPane.setHgap(10);
+
+
+        // i butonave
 
         hBox.getChildren().addAll(btnSearch,btnAdd,btnUpdate);
         hBox.setAlignment(Pos.CENTER);
         hBox.setPadding(new Insets(10,10,10,10));
         hBox.setSpacing(10);
 
+        // i krejt panes n'tmajt
         leftPane.getChildren().addAll(formPane,hBox);
-        leftPane.setAlignment(Pos.CENTER);
+        leftPane.setPadding(new Insets(10));
+
+
+
+
         TableColumn<String, Book> col1 = new TableColumn<>("Vendi i Nisjes");
         col1.setCellValueFactory(new PropertyValueFactory<>("start"));
         col1.setPrefWidth(100);
@@ -63,6 +92,7 @@ public class CompaniesUI  extends HBox{
         TableColumn<Button,Book> col6 = new TableColumn<>("Delete");
         col6.setCellValueFactory(new PropertyValueFactory<>("btnDelete"));
         col6.setPrefWidth(50);
+
         tabela.getColumns().addAll(col1, col2, col3, col4, col5,col6);
 
 
