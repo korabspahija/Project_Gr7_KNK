58f1ef6821fc9d30ca8206859e8d08c7624b740b
Vegim-Bytyqi <44346620+Vegim-Bytyqi@users.noreply.github.com>
2019-06-12 21:40:53 +0200

    Menu Strip

--- Views/AdminView.java
+++ Views/AdminView.java
@@ -1,6 +1,5 @@
+import javafx.application.Platform;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.*;
@@ -14,6 +13,49 @@ public class AdminView extends BorderPane {
     private Button btnSchedule= new Button("Modify Schedule");
 	
 	public AdminView() {
+		
+		// General Menu
+        Menu generalMenu = new Menu("File"); 
+        
+        MenuItem exitMenuItem = new MenuItem("Exit"); 
+        
+        MenuItem backMenuItem = new MenuItem("Back"); 
+        
+        exitMenuItem.setOnAction(e -> {
+        	Platform.exit();
+        });
+        
+        generalMenu.getItems().addAll(backMenuItem,exitMenuItem); 
+
+        
+        // HelpMenu
+        Menu helpMenu = new Menu("Help") ;
+       
+        MenuItem aboutHelpItem = new MenuItem("About"); 
+        helpMenu.getItems().add(aboutHelpItem); 
+        
+        Menu languagesMenu = new Menu("Languages");
+        
+        MenuItem english = new MenuItem("English");
+        
+        MenuItem albanian = new MenuItem("Albanian"); 
+        languagesMenu.getItems().addAll(english, albanian);
+        
+        aboutHelpItem.setOnAction(e -> {
+        	Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("About us");
+            alert.setHeaderText("We hope you are satisfied with our programm");
+            alert.setContentText("We worked very hard and i hope u like our program");
+            alert.showAndWait();
+        });
+        
+  
+        MenuBar mb = new MenuBar(); 
+  
+        mb.getMenus().addAll(generalMenu, helpMenu,languagesMenu); 
+  
+        VBox vbMenu = new VBox(mb);
+		
 		VBox vBox = new VBox(15);
 		
 	    vBox.setPadding(new Insets(15, 15, 15, 15));
@@ -30,17 +72,17 @@ public class AdminView extends BorderPane {
 	    btnSchedule.setPrefHeight(140);
 	    btnSchedule.setPrefWidth(300);
 	    
-	    HBox hbox = new HBox(10);
 	    TextField field = new TextField();
 	    HBox.setHgrow(field, Priority.ALWAYS);
-	    Button btnBack = new Button("Back");
-	    hbox.getChildren().addAll(btnBack,new Label("Search:"), field, new Button("Go"));
+	    
 	     
 	    vBox.getChildren().addAll(btnUsers,btnCities,btnLines,btnSchedule);
 	    setLeft(vBox);
-	    setTop(hbox);	
+	    setTop(vbMenu);	
 	    
-	    btnBack.setOnAction(e->{
+	    
+	    
+	    backMenuItem.setOnAction(e->{
 	    	setRight(new HBox());
 	    });
 	    
